f53731a1370ffdadc25a70e76504c24dc84c4a4e
swqs1989 <myqswqs@gmail.com>
2017-02-21 00:43:35 -0500

    done

---
+++ src/project2/JSONInputFormat.java
@@ -0,0 +1,16 @@
+package project2;
+
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.RecordReader;
+import org.apache.hadoop.mapreduce.TaskAttemptContext;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+
+import java.io.IOException;
+
+/**
+ * Created by Youqiao Ma on 2/20/2017.
+ */
+public class JSONInputFormat{
+
+}
